b584f80af74901060d712486e16b76f2c986a343
Attila Olbrich <olbrichattila@gmx.co.uk>
2021-12-19 14:01:26 +0000

    Add readme

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# Getting Started with Create React App
+# React APP, drawing animated sprites for ZX-SPECTRUM
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
+## Available Scripts to build
 
 In the project directory, you can run:
 
@@ -14,11 +12,6 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
 ### `yarn build`
 
 Builds the app for production to the `build` folder.\
@@ -29,42 +22,16 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `yarn eject`
+# Application can draw animated sprites for ZX spectrum, and generates the define statements for your assembly code.
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+It is possible to draw 8 frames and generate the code what you can use in your assembly sprite manager.
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+Example tutorial game here:
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+You can also copy back your assemby code and make changes
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+Screensot:
 
-## Learn More
+![Screen](https://github.com/olbrichattila/zx-spectrum-demo-assembly-game/blob/main/images/game-screen.png?raw=true)
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Enjoy
--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import {
+  getSpriteData,
+  convertSpriteDataToText,
+  parseSpriteData,
+} from "./helpers/sprite";
+import { getDefaultMatrix, getDefaultFrames } from "./helpers/editor";
+import Navigator from "./components/navigator";
+import SizeSelector from "./components/sizeSelector";
+import FrameSelector from "./components/frameSelector";
+import SpriteEditor from "./components/spriteEditor";
+import SpritePreview from "./components/spritePreview";
+import Error from "./components/error";
+import FrameDataEditor from "./components/frameDataEditor";
+import "./App.scss";
 
 function App() {
+  const [currentWidth, setCurrentWidth] = useState(16);
+  const [currentHeight, setCurrentHeight] = useState(16);
+  const [spriteName, setSpriteName] = useState("frame");
+  const [frames, setFrames] = useState(
+    getDefaultFrames(currentWidth, currentHeight)
+  );
+  const [selectedFrameId, setSelectedFrameId] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [frameCopyBuffer, setFrameCopyBuffer] = useState(
+    getDefaultMatrix(currentWidth, currentHeight)
+  );
+  const [spriteDataText, setSpriteDataText] = useState("");
+  const [error, setError] = useState();
+
+  const onReset = () => {
+    setFrames(getDefaultFrames(currentWidth, currentHeight));
+  };
+
+  const onSpriteEditorChange = (matrix) => {
+    const newFrames = [...frames];
+    newFrames[selectedFrameId] = matrix;
+    setFrames(newFrames);
+  };
+
+  // if play button pressed, rotate frames
+  if (isPlaying) {
+    setTimeout(() => {
+      const newFrameId = selectedFrameId === 7 ? 0 : selectedFrameId + 1;
+      setSelectedFrameId(newFrameId);
+    }, 50);
+  }
+
+  // reset data if widht or height changes
+  useEffect(() => {
+    setFrames(getDefaultFrames(currentWidth, currentHeight));
+    setSpriteDataText("");
+  }, [currentWidth, currentHeight]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>ZX Spectrum sprite editor</h1>
+      <SizeSelector
+        onWidthChange={(width) => setCurrentWidth(width)}
+        onHeightChange={(height) => setCurrentHeight(height)}
+      />
+      <div className="preview-box">
+        <span>Sprite Name:</span>
+        <input
+          type="text"
+          value={spriteName}
+          onChange={(event) => setSpriteName(event.target.value)}
+        />
+      </div>
+      <Navigator
+        isPlaying={isPlaying}
+        onResetClick={() => onReset()}
+        onPlayFramesClick={() => setIsPlaying(!isPlaying)}
+        onCopyFrameClick={() => setFrameCopyBuffer(frames[selectedFrameId])}
+        onPasteFrameClick={() => onSpriteEditorChange(frameCopyBuffer)}
+        onParseDataClick={() => {
+          const parsedData = parseSpriteData(spriteDataText);
+          if (Array.isArray(parsedData) && parsedData.length === 8) {
+            setFrames(parsedData);
+          } else {
+            setError(
+              "Cannot parse inputted data, please check if the dimensions matching your source data (width, height)."
+            );
+          }
+        }}
+        onGetSpriteDataClick={() => {
+          const data = getSpriteData(frames, currentWidth);
+          setSpriteDataText(convertSpriteDataToText(spriteName, data));
+        }}
+      />
+      <Error message={error} onHide={() => setError(null)} />
+      <div className="preview-box">
+        <span>Preview:</span>
+        <SpritePreview
+          width={currentWidth}
+          height={currentHeight}
+          matrix={[...frames[selectedFrameId]]}
+        />
+      </div>
+      <div className="editors">
+        <SpriteEditor
+          width={currentWidth}
+          height={currentHeight}
+          matrix={[...frames[selectedFrameId]]}
+          onChange={(matrix) => onSpriteEditorChange(matrix)}
+        />
+
+        <FrameSelector
+          selectedFrameId={selectedFrameId}
+          onChangeFrameId={(frameId) => setSelectedFrameId(frameId)}
+        />
+      </div>
+      <FrameDataEditor
+        text={spriteDataText}
+        onChange={(text) => setSpriteDataText(text)}
+      />
+    </>
   );
 }
 
